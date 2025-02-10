e266e4cefb81812e7fe9171cd7b4637773bac395
Harsh Mathur <harsh.mathur@mobileprogramming.com>
2025-02-10 23:09:01 +0530

    enquiryForm created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -105,7 +105,7 @@ const Navbar = () => {
                   </select>
                 </div>
               </li>
-            </ul>
+            </ul> 
             {/* Destination Dropdown */}
           </div>
         </div>
